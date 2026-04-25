96593ece5d02d318e342dece7c05f5f7d1f4e8f7
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-04-25 10:37:23 -0300

    Update name_cases.py
    Removing unnecessary comments.

--- python_work/variables/name_cases.py
+++ python_work/variables/name_cases.py
@@ -1,25 +1,25 @@
-# Armazena o nome do usuário e cria uma mensagem de saudação personalizada
+# Mensagem pessoal
 name = "Guilherme"
-message = "Olá " + name + "! Vamos escrever um pouco de código Python hoje?"
+message = "Alô " + name + ", você gostaria de  aprender um pouco de Python hoje?"
 print(message)
 
-# Demonstra diferentes formas de formatação de texto (minúsculas, maiúsculas e título)
-name = "Maria clara"
-print(name.lower())   # Converte para letras minúsculas
-print(name.upper())   # Converte para letras maiúsculas
-print(name.title())   # Coloca a primeira letra de cada palavra em maiúscula
+# Letras maiúsculas e minúsculas em nomes
+name = "Ada Lovelace"
+print(name.lower())
+print(name.upper())  
+print(name.title())
 
-# Exibe uma citação famosa diretamente no código
-print('Albert Einstein certa vez disse: "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original."')
+# Citações famosas
+message = 'Albert Einstein certa vez disse: "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original".'
+print(message)
 
-# Armazena o nome de uma pessoa famosa e monta uma citação com concatenação
 famous_person = "Robert Oppenheimer"
-message = '"Agora eu me tornei a Morte, a destruidora de mundos." – ' + famous_person
+message = '"O otimista pensa que este é o melhor de todos os mundos possíveis. O pessimista receia que isso seja verdade." – ' +famous_person
 print(message)
 
-# Demonstra o uso de caracteres de espaço (\t e \n) e remoção de espaços extras
-name = "\t Alan Turing \n"
-print(name)   # Exibe o texto com espaços e quebras de linha
-print(name.lstrip())  # Remove espaços à esquerda
-print(name.rstrip())  # Remove espaços à direita
-print(name.strip())   # Remove espaços dos dois lados
+# Removendo espaços em branco
+name = "\tAlan Turing\n"
+print(name)
+print(name.lstrip())
+print(name.rstrip())
+print(name.strip()) 
