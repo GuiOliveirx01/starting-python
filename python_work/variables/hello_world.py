4deb5c9daef04117799690e068cd8b76efbb9688
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-04-25 10:29:55 -0300

    Update hello_world.py
    Removing unnecessary comments.

--- python_work/variables/hello_world.py
+++ python_work/variables/hello_world.py
@@ -1,14 +1,10 @@
-# Exibe uma mensagem na tela usando a função print()
 print("Hello Python world!")
 
-# Armazena a mensagem em uma variável e exibe a mensagem na tela
 message = "Hello Python world!"
 print(message)
 
-# Atualiza o valor da variável com uma nova mensagem e exibe o novo conteúdo
 message = "Hello Python Crash Course world!"
 print(message)
 
-# Evita um erro de nome ao escrever o segundo 's' em 'print(message)'
 message = "Hello Python Crash Course reader!"
 print(message)
