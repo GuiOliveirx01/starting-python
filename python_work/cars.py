a0ae71645a7b352a8d9ac2441e5ac4fc313d9806
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-04-25 10:42:42 -0300

    Create cars.py

---
+++ python_work/cars.py
@@ -0,0 +1,13 @@
+cars = ['lamborghini', 'ferrari', 'bmw', 'porsche']
+
+message = "Acho os modelos da " + cars[0].title() + " muito bonitos.\n"
+print(message)
+
+message = "Gostaria de ter um carro da " + cars[1].title() + ".\n"
+print(message)
+
+message = "Sempre fico admirando quando passa uma " + cars[2].upper() + " na rua.\n"
+print(message)
+
+message = "A " + cars[3].title() + " tem carros bem estilosos." 
+print(message)
