3a15935a63ad32db98b2d02dac5f56cc96e272dc
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-04-25 10:38:30 -0300

    Update numbers.py
    Removing unnecessary comments.

--- python_work/variables/numbers.py
+++ python_work/variables/numbers.py
@@ -1,15 +1,11 @@
-# Demonstra diferentes operações matemáticas (soma, subtração, multiplicação e divisão) 
-# que resultam no mesmo valor: 8
-print(4 + 4)   # Soma
-print(10 - 2)  # Subtração
-print(2 * 4)   # Multiplicação
-print(16 / 2)  # Divisão
+# Guilherme Oliveira – 23/04/2026
+# Este programa exibe operações com inteiros e uma mesagem que converte um inteiro para uma string
 
-# Armazena o número favorito em uma variável
-favorite_number = 2015
+print(5 + 3)
+print(14 - 6)
+print(4 * 2)
+print(16 / 2)
 
-# Evita erro de tipo ao converter o número para string e monta uma frase personalizada
-message = "My favorite number is " + str(favorite_number) + "!"
-
-# Exibe a mensagem final no console
+number = 8128
+message = "Meu número favorito é " + str(number) + "!"
 print(message)
