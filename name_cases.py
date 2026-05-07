a88fd9d4c6811ddd979ab9062714cce77dcb353b
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-07 06:42:21 -0300

    Create name_cases.py
    pequenos programas que usam diferentes métodos de manipulação de strings

---
+++ name_cases.py
@@ -0,0 +1,19 @@
+user_name = "Guilherme"
+print(f"Alô {user_name}, você gostaria de aprender um pouco de Python hoje?")
+
+scientist_name = "Ada Lovelace"
+print(scientist_name.lower())
+print(scientist_name.upper())  
+print(scientist_name.title())
+
+print('Albert Einstein certa vez disse: "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original."')
+
+famous_person = "Robert Oppenheimer"
+message = f'"O otimista pensa que este é o melhor de todos os mundos possíveis. O pessimista receia que isso seja verdade." – {famous_person}'
+print(message)
+
+another_name = "\tAlan Turing\n"
+print(another_name)
+print(another_name.lstrip())
+print(another_name.rstrip())
+print(another_name.strip())
