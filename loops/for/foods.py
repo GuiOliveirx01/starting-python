4c47de474e8988cf0edfc5910f142818106fa2cb
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-09 13:59:50 -0300

    feat: adiciona arquivo foods.py com listas de comidas favoritas

---
+++ loops/for/foods.py
@@ -0,0 +1,13 @@
+my_foods = ['pizza', 'strogonnoff','chocolate']
+friend_foods = my_foods[:]
+
+my_foods.append('batata frita')
+friend_foods.append('sorvete')
+
+print("Minhas comidas favoritas são:")
+for food in my_foods:
+	print(f"- {food}")
+
+print("\nAs comidas favoritas do meu amigo são:")
+for food in friend_foods:
+	print(f"- {food}")
