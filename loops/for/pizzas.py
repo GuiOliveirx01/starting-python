25b3221ce0f94547c35c8b4faca7bacef70691f3
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-09 13:41:23 -0300

    feat: introduz como copiar uma lista em pizzas.py

--- loops/for/pizzas.py
+++ loops/for/pizzas.py
@@ -4,3 +4,15 @@ for pizza in pizzas:
 	print(f"Gosto de pizza sabor {pizza}.")
 	
 print("Eu realmente adoro pizza!")
+
+friends_pizzas = pizzas[:]
+pizzas.append('portuguesa')
+friends_pizzas.append('marguerita')
+
+print("\nMinhas pizzas favoritas são:")
+for pizza in pizzas:
+	print(f"- {pizza}")
+
+print("\nAs pizzas favoritas do meu amigo são:")
+for pizza in friends_pizzas:
+	print(f"- {pizza}")
