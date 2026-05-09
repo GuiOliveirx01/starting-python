0aa757f225c03bef4e379c8c8a887ca7c9bf6696
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-09 13:37:08 -0300

    feat: introduz fatias adicionando algumas a animals.py

--- loops/for/animals.py
+++ loops/for/animals.py
@@ -1,6 +1,17 @@
-animals = ['hipopótamo', 'elefante', 'rinoceronte']
-
+animals = ['hipopótamo', 'elefante', 'rinoceronte', 'girafa', 'zebra']
 for animal in animals:
-	print(f"O {animal} é um grande herbívoro.")
+	print(f"O {animal} é um mamífero.")
 	
 print("Todos eles comem toneladas de plantas e frutas!")
+
+print("\nOs três primeiros animais da lista são:")
+for animal in animals[0:3]:
+	print(f"- {animal}")
+
+print("\nOs três animais do meio da lista são:")
+for animal in animals[1:4]:
+	print(f"- {animal}")
+
+print("\nOs três últimos animais da lista são:")
+for animal in animals[-3:]:
+	print(f"- {animal}")
