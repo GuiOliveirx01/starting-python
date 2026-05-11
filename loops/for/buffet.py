f2791284ecd0090561f49461a1c076cbd6905cdb
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-11 12:15:17 -0300

    feat: adiciona arquivo buffet.py que introduz tuplas

---
+++ loops/for/buffet.py
@@ -0,0 +1,13 @@
+dishes = ("misto quente", "omelete", "salada", "sanduíche natural", "asinhas de frango")
+
+print("O buffet oferece os seguintes pratos:")
+for dish in dishes:
+    print(f"- {dish}")
+
+# dishes[0] = "pizza"  # This will raise a TypeError since tuples are immutable.
+
+dishes = ("pizza", "omelete", "salada", "fritas com cheddar e bacon", "asinhas de frango")  # To change the dishes, we need to create a new tuple.
+
+print("\nO buffet agora oferece os seguintes pratos:")
+for dish in dishes:
+    print(f"- {dish}")
