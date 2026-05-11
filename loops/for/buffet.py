7b1d6873c3c7fdc4d6246dd0bedbe66a7db80677
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-11 12:55:48 -0300

    style: identação em programas que ultrapassam o limite de 79 caracteres

--- loops/for/buffet.py
+++ loops/for/buffet.py
@@ -1,13 +1,20 @@
-dishes = ("misto quente", "omelete", "salada", "sanduíche natural", "asinhas de frango")
+dishes = (
+    "misto quente", "omelete", "salada", "sanduíche natural", 
+    "asinhas de frango"
+    )
 
 print("O buffet oferece os seguintes pratos:")
 for dish in dishes:
     print(f"- {dish}")
 
-# dishes[0] = "pizza"  # This will raise a TypeError since tuples are immutable.
+# This will raise a TypeError since tuples are immutable.
+# dishes[0] = "pizza"  
 
 # To change the dishes, we need to create a new tuple.
-dishes = ("pizza", "omelete", "salada", "fritas com cheddar e bacon", "asinhas de frango")
+dishes = (
+    "pizza", "omelete", "salada", "fritas com cheddar e bacon", 
+    "asinhas de frango"
+    )
 
 print("\nO buffet agora oferece os seguintes pratos:")
 for dish in dishes:
--- variables/name_cases.py
+++ variables/name_cases.py
@@ -6,10 +6,14 @@ print(scientist_name.lower())
 print(scientist_name.upper())  
 print(scientist_name.title())
 
-print('Albert Einstein certa vez disse: "A mente que se abre a uma nova ideia jamais voltará ao seu tamanho original."')
+print(
+    'Albert Einstein certa vez disse: "A mente que se abre a uma nova ideia ' \
+    'jamais voltará ao seu tamanho original."'
+    )
 
 famous_person = "Robert Oppenheimer"
-message = f'"O otimista pensa que este é o melhor de todos os mundos possíveis. O pessimista receia que isso seja verdade." – {famous_person}'
+message = f'"O otimista pensa que este é o melhor de todos os mundos ' \
+f'possíveis. O pessimista receia que isso seja verdade." – {famous_person}'
 print(message)
 
 another_name = "\tAlan Turing\n"
