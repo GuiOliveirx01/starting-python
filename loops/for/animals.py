2297133a7fbf4b25d1a9f32fb0ae9d0794cb74bf
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-13 21:18:02 -0300

    style: alinhando animals.py com a PEP 8

--- loops/for/animals.py
+++ loops/for/animals.py
@@ -1,17 +1,27 @@
-animals = ['hipopótamo', 'elefante', 'rinoceronte', 'girafa', 'zebra']
+animals = [
+    'hipopótamo',
+    'elefante',
+    'rinoceronte',
+    'búfalo',
+    'gnu',
+]
+
 for animal in animals:
-	print(f"O {animal} é um mamífero.")
-	
+    print(f"O {animal} é um mamífero.")
+
 print("Todos eles comem toneladas de plantas e frutas!")
 
 print("\nOs três primeiros animais da lista são:")
+
 for animal in animals[0:3]:
-	print(f"- {animal}")
+    print(f"- {animal}")
 
 print("\nOs três animais do meio da lista são:")
+
 for animal in animals[1:4]:
-	print(f"- {animal}")
+    print(f"- {animal}")
 
 print("\nOs três últimos animais da lista são:")
+
 for animal in animals[-3:]:
-	print(f"- {animal}")
+    print(f"- {animal}")                                                                        
