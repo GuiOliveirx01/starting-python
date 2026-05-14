60b6a47617340e4330dd711d9ea38bea2384eb12
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-13 21:59:32 -0300

    style: alinhando outros programas da pasta loops com a PEP 8

--- loops/for/pizzas.py
+++ loops/for/pizzas.py
@@ -1,4 +1,8 @@
-pizzas = ['calabresa', 'cheddar e bacon', 'frango com catupiry']
+pizzas = [
+	'calabresa',
+	'cheddar e bacon', 
+	'frango com catupiry'
+]
 
 for pizza in pizzas:
 	print(f"Gosto de pizza sabor {pizza}.")
@@ -6,13 +10,16 @@ for pizza in pizzas:
 print("Eu realmente adoro pizza!")
 
 friends_pizzas = pizzas[:]
+
 pizzas.append('portuguesa')
 friends_pizzas.append('marguerita')
 
 print("\nMinhas pizzas favoritas são:")
+
 for pizza in pizzas:
 	print(f"- {pizza}")
 
 print("\nAs pizzas favoritas do meu amigo são:")
+
 for pizza in friends_pizzas:
 	print(f"- {pizza}")
