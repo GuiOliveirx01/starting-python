4aeb9f39dfde1fce5044436a6c0605854495f71d
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-18 13:14:50 -0300

    Update guests.py
    refactor: muda nome da variáveis para melhor semântica

--- lists/guests.py
+++ lists/guests.py
@@ -1,47 +1,47 @@
 # ===== PRIMEIROS CONVITES =====
-names = [
-    'maria clara', 
-    'victor', 
+guests = [
+    'maria clara',
+    'victor',
     'isaac'
 ]
 
-for name in names:
-    print(f"Olá {name.title()}! Você está convidado(a) para um jantar hoje às 21h!")
+for guest in guests:
+    print(f"Olá {guest.title()}! Você está convidado(a) para um jantar hoje às 21h!")
 
 # ===== SUBSTITUINDO CONVIDADO =====
-print(f"\n{names[2].title()} não irá comparecer.\n")
+print(f"\n{guests[2].title()} não irá comparecer.\n")
 
-names[2] = 'gabriel'
+guests[2] = 'gabriel'
 
-for name in names:
-    print(f"Olá {name.title()}! Você está convidado(a) para um jantar hoje às 21h!")
+for guest in guests:
+    print(f"Olá {guest.title()}! Você está convidado(a) para um jantar hoje às 21h!")
 
-# ==== ACRESCENTANDO CONVIDADOS ====
+# ===== ACRESCENTANDO CONVIDADOS =====
 print("\nGalera! Encontrei uma mesa de jantar que cabe mais pessoas!\n")
 
-names.insert(0, 'ana beatriz')
-names.insert(2, 'marcelo')
-names.append('valeska')
+guests.insert(0, 'ana beatriz')
+guests.insert(2, 'marcelo')
+guests.append('valeska')
 
-print(f"Agora estou convidando {len(names)} pessoas para o jantar!\n")
+print(f"Agora estou convidando {len(guests)} pessoas para o jantar!\n")
 
-for name in names:
-    print(f"Olá {name.title()}! Você está convidado(a) para um jantar hoje às 21h!")
+for guest in guests:
+    print(f"Olá {guest.title()}! Você está convidado(a) para um jantar hoje às 21h!")
 
 # ===== REDUZINDO CONVIDADOS =====
 print("\nInfelizmente a mesa não ficou pronta e só consigo convidar duas pessoas.\n")
 
 # Remove nomes até restarem apenas dois convidados
-while len(names) > 2:
-    removed = names.pop()
-    print(f"Sinto muito por não poder te convidar {removed.title()}.")
+while len(guests) > 2:
+    removed_guest = guests.pop()
+    print(f"Sinto muito por não poder te convidar {removed_guest.title()}.")
 
 print()
 
-for name in names:
-    print(f"{name.title()}, você ainda está convidado(a)!")
+for guest in guests:
+    print(f"{guest.title()}, você ainda está convidado(a)!")
 
 # Remove todos os nomes da lista
-del names[:]
+del guests[:]
 
-print(names)
+print(guests)
