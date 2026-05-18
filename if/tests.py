48cfc23942a0d8f6c34d4bc4c45588a3cbd732b0
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-18 20:28:36 -0300

    feat: create test.py for conditional tests

---
+++ if/tests.py
@@ -0,0 +1,23 @@
+car = 'ford'
+
+print("Is car == 'ford'? I predict True.")
+print(car == 'ford')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
+
+food = 'pizza'
+
+print("\nIs food == 'pizza'? I predict True.")
+print(food == 'pizza')
+
+print("\nIs food == 'cake'? I predict False.")
+print(food == 'cake')
+
+topping = 'mushrooms'
+
+print("\nIs topping == 'mushrooms'? I predict True.")
+print(topping == 'mushrooms')
+
+print("Is topping == 'pepperoni'? I predict False")
+print()
