083fcd6641b7454728e5e258e74f66b88162d459
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-18 21:33:09 -0300

    docs: add comment in foods.py

--- loops/for/foods.py
+++ loops/for/foods.py
@@ -1,9 +1,10 @@
 my_foods = [
-	'pizza', 
-	'strogonnoff',
-	'chocolate'
+    'pizza',
+    'strogonoff',
+    'chocolate'
 ]
 
+# Creates a copy of the list instead of sharing the same reference
 friend_foods = my_foods[:]
 
 my_foods.append('batata frita')
@@ -12,9 +13,9 @@ friend_foods.append('sorvete')
 print("Minhas comidas favoritas são:")
 
 for food in my_foods:
-	print(f"- {food}")
+    print(f"- {food}")
 
 print("\nAs comidas favoritas do meu amigo são:")
 
 for food in friend_foods:
-	print(f"- {food}")
+    print(f"- {food}")
