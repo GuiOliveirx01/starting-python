96e3936dd56e5b2b8d7887573fce3c090046916c
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-18 22:30:39 -0300

    style: convert indentation to spaces in pizzas.py

--- lists/pizzas.py
+++ lists/pizzas.py
@@ -1,12 +1,12 @@
 pizzas = [
-	'calabresa',
-	'cheddar e bacon', 
-	'frango com catupiry'
+    'calabresa',
+    'cheddar e bacon', 
+    'frango com catupiry'
 ]
 
 for pizza in pizzas:
-	print(f"Gosto de pizza sabor {pizza}.")
-	
+    print(f"Gosto de pizza sabor {pizza}.")
+    
 print("Eu realmente adoro pizza!")
 
 friends_pizzas = pizzas[:]
@@ -17,9 +17,9 @@ friends_pizzas.append('marguerita')
 print("\nMinhas pizzas favoritas são:")
 
 for pizza in pizzas:
-	print(f"- {pizza}")
+    print(f"- {pizza}")
 
 print("\nAs pizzas favoritas do meu amigo são:")
 
 for pizza in friends_pizzas:
-	print(f"- {pizza}")
+    print(f"- {pizza}")
