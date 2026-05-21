0668475543d53473de48081ab7a8d79861e7a42b
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-20 22:00:14 -0300

    refactor: update conditional_tests.py

---
+++ conditionals/conditional_tests.py
@@ -0,0 +1,17 @@
+# Teste de igualdade e não igualdade
+car = 'ford'
+
+print(car == 'ford')
+print(car != 'ford')
+
+# Teste para ignorar letras maiúsculas e minúsculas
+car = 'Porsche'
+
+print(car.lower() == 'porsche')
+print(car == 'porsche')
+
+# Testes númericos
+favorite_number = 42
+
+print(favorite_number == 42)
+print(favorite_number != 42)
