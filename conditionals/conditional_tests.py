ed9592b7ba70a06a4d7622e1088b63d69888c124
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-20 22:40:37 -0300

    refactor: adding conditional tests

---
+++ conditionals/conditional_tests.py
@@ -0,0 +1,22 @@
+car = 'subaru'
+
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'ford'? I predict False.")
+print(car == 'ford')
+
+# Equality and unequal with strings
+car = 'porsche'
+
+print("\nIs car == 'porsche'? I predict True.")
+print(car == 'porsche')
+
+print("\nIs car != 'porsche'? I predict False.")
+print(car != 'porsche')
+
+# Using the lower() function
+car = 'Audi'
+
+print("\nIs car.lower() == 'audi? I predict True.")
+print(car.lower() == 'audi')
