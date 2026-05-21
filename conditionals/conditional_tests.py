03115b75bd94ccf13c9a74f0b355139b46cd2ed7
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-21 07:04:09 -0300

    refactor: adding more conditional tests in conditional_tests.py

--- conditionals/conditional_tests.py
+++ conditionals/conditional_tests.py
@@ -1,12 +1,13 @@
-car = 'subaru'
+# Animals
+animal = 'shark'
 
-print("Is car == 'subaru'? I predict True.")
-print(car == 'subaru')
+print("Is animal == 'shark'? I predict True.")
+print(animal == 'shark')
 
-print("\nIs car == 'ford'? I predict False.")
-print(car == 'ford')
-
-# Equality and unequal with strings
+print("\nIs animal == 'dolphin'? I predict False.")
+print(animal == 'dolphin')
+ 
+# Cars
 car = 'porsche'
 
 print("\nIs car == 'porsche'? I predict True.")
@@ -15,8 +16,59 @@ print(car == 'porsche')
 print("\nIs car != 'porsche'? I predict False.")
 print(car != 'porsche')
 
-# Using the lower() function
-car = 'Audi'
+# Foods
+food = 'Pizza'
 
-print("\nIs car.lower() == 'audi? I predict True.")
-print(car.lower() == 'audi')
+print("\nIs food.lower() == 'pizza'? I predict True.")
+print(food.lower() == 'pizza')
+
+print("\nIs food == 'pizza'? I predict False.")
+print(food == 'pizza')
+
+# Favorite numbers
+favorite_number = 10
+
+print("\nIs favorite_number == 10? I predict True.")
+print(favorite_number == 10)
+
+print("\nIs favorite_number == 12? I predict False.")
+print(favorite_number == 12)
+
+print("\nIs favorite_number > 8? I predict True.")
+print(favorite_number > 8)
+
+print("\nIs favorite_number < 9? I predict False.")
+print(favorite_number < 9)
+
+print("\nIs favorite_number >= 10? I predict True.")
+print(favorite_number >= 10)
+
+print("\nIs favorite_number <= 6? I predict False.")
+print(favorite_number <= 6)
+
+# Ages
+age_0 = 19
+age_1 = 19
+
+print("\nIs (age_0 >= 18) and (age_1 >= 18)? I predict True.")
+print((age_0 >= 18) and (age_1 >= 18))
+
+age_0 = 21
+
+print("\nIs (age_0 >= 21) and (age_1 >= 21)? I predict False.")
+print((age_0 >= 21) and (age_1 >= 21))
+
+print("\nIs (age_0 >= 21) or (age_1 >= 21)? I predict True.")
+print((age_0 >= 21) or (age_1 >= 21))
+
+print("\nIs (age_0 >= 22) or (age_1 >= 22)? I predict False.")
+print((age_0 >= 22) or (age_1 >= 22))
+
+# Languages
+languages = ['python', 'java', 'c#']
+
+print("\nIs 'python' in languages? I predict True.")
+print('python' in languages)
+
+print("\nIs 'java' not in languages? I predict False.")
+print('java' not in languages)
