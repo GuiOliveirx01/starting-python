3adc0b11da332ec402715ae73f7454e4356631ea
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-23 11:41:00 -0300

    feat: create program using numerical comparisons, user input and string conversion with `int()`

---
+++ conditionals/life_stages.py
@@ -0,0 +1,14 @@
+age = int(input("How old are you? "))
+
+if age < 2:
+    print("You're a baby.")
+elif age < 4:
+    print("You're a child.")
+elif age < 13:
+    print("You're a kid.")
+elif age < 20:
+    print("You're a teenager.")
+elif age < 65:
+    print("You're a adult.")
+else:
+    print("You're a elderly.")
