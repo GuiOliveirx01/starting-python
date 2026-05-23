6ec159b9067371ad9983701edf18d107740bbd2e
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-23 12:15:00 -0300

    feat: create favorite_fruits.py

---
+++ conditionals/favorite_fruits.py
@@ -0,0 +1,16 @@
+favorite_fruits = ['banana', 'apple', 'watermelon', 'orange', 'tangerine']
+
+if 'banana' in favorite_fruits:
+    print("You really like bananas!")
+    
+if 'apple' in favorite_fruits:
+    print("You really like apples!")
+    
+if 'watermelon' in favorite_fruits:
+    print("You really like watermelons!")
+   
+if 'orange' in favorite_fruits:
+    print("You really like oranges!")
+    
+if 'tangerine' in favorite_fruits:
+    print("You really like tangerines!")
