9ffa74c56b97bee06cbad361e0d7a14ba614112f
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-25 11:04:39 -0300

    feat(users.py): complete exercise

--- conditionals/users.py
+++ conditionals/users.py
@@ -1,2 +1,9 @@
-current_users = ['guilherme', 'maria', 'victor', 'gabriel', 'júlio']
-new_users = ['ana', 'ketlei', 'pedro', 'guilherme', 'maria']
+current_users = ['Guilherme', 'Maria', 'Gabriel', 'Ana', 'Marcelo']
+
+new_users = ['Pedro', 'Lucas', 'Mateo', 'Marcelo', 'Ana']
+
+for new_user in new_users:
+    if new_user.title() in current_users:
+        print("Este nome já está sendo usado, por favor selecione outro nome.")
+    else:
+        print("Este nome está disponível.")
