76f1540ed472ce53274470d9dcbed467709c7784
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-25 20:40:53 -0300

    feat: add program introducing dictionaries

---
+++ dictionaries/person.py
@@ -0,0 +1,9 @@
+person = {
+    'first_name': 'maria clara',
+    'last_name': 'viana',
+    'age': '19',
+    'city': 'belo horizonte',
+}
+
+print(f"My girlfriend's name is {person['first_name'].title()}"
+    f"{person['last_name'].title()}, she's 18. She lives in Belo Horizonte.")
