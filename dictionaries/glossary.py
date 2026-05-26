eef46355c43baf9c8b721b23f474a5a182867109
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-25 22:02:48 -0300

    feat(dictionaries): add glossary.py

---
+++ dictionaries/glossary.py
@@ -0,0 +1,10 @@
+glossary = {
+    'Variáveis': 'É um espaço para armazenar dados (valores).',
+    'Listas': 'É um tipo de variável que armazena um conjunto de dados.',
+    'Laços de repetição': 'É uma estrutura para comandos de repetição.',
+    'Tuplas': 'É um tipo de variável em que os dados nela são imutáveis.',
+    'Condições': 'É uma estrutura para comandos que envolvem comparações.',
+}
+
+for key, value in glossary.items():
+    print(f"{key}:\n{value}\n")
