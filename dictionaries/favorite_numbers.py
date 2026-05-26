67db8402c07f192e3c564e40b8abb2ffe9cf6c70
GuiOliveirx01 <guilhermeoliveiradearaujo015@gmail.com>
2026-05-25 21:35:14 -0300

    feat: add program using dictionaries and loops

---
+++ dictionaries/favorite_numbers.py
@@ -0,0 +1,10 @@
+favorite_numbers = {
+    'maria': 7, 
+    'guilherme': 14,
+    'arthur': 57,
+    'erik': 39,
+    'alexandre': 42,
+}
+
+for key, value in favorite_numbers.items():
+    print(f"{key.title()}'s favorite number is {str(value)}.")
